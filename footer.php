a169d6fb4e01fb4d7c6a0145b917540e43dc8b49
subhanghani339 <subhanghani339@gmail.com>
2022-06-07 18:02:03 +0500

    5 commit

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@ wp_footer();
 ?>
 
 <!-- Footer -->
-<footer class="text-center text-lg-start bg-light text-muted">
+<footer class="text-center text-lg-start bg-dark text-muted">
   <!-- Section: Social media -->
   <section
     class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
@@ -50,7 +50,7 @@ wp_footer();
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <!-- Content -->
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
+            <i class="fas fa-gem me-3"></i>Ansty
           </h6>
           <p>
             Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -137,3 +137,9 @@ wp_footer();
   <!-- Copyright -->
 </footer>
 <!-- Footer -->
+
+<style>
+     p{
+        color: white;
+    }
+</style>
--- header.php
+++ header.php
@@ -5,9 +5,9 @@ wp_head();
 
 ?>
 
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/WordpressPractice">Navbar</a>
+    <a class="navbar-brand" href="/WordpressPractice">Ansty</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
