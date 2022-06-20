b7e0a77fe13b605843435de5c020ab1ffd626e6e
subhanghani339 <subhanghani339@gmail.com>
2022-06-20 11:29:23 +0500

    comment.php file added

--- footer.php
+++ footer.php
@@ -131,7 +131,7 @@ wp_footer();
 
   <!-- Copyright -->
   <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2021 Copyright:
+    © 2022 Copyright:
     <a class="text-reset fw-bold" href="https://subhanghani.com/">SubhanGhani.com</a>
   </div>
   <!-- Copyright -->
--- header.php
+++ header.php
@@ -7,7 +7,7 @@ wp_head();
 
 <nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/WordpressPractice">Ansty</a>
+    <a class="navbar-brand" href="/WordpressPracticeAgain">Ansty</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
